70abd27aaf70852a5cb9e7a8cba73068c737f729
adept-bell-4492 <garlandegoma@gmail.com>
2020-06-23 19:00:56 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,8 @@
 # code your #valid_move? method here
 def valid_move?(board,index)
   
- if  index.between?(1,9) && position_taken?(board, index) && board[index]== " " && 
+ if  index.between?(1,9) && position_taken?(board, index) && board[index]== " " && board[index] == "" && board[index] == nil
+     false
    true
   else 
     nil
